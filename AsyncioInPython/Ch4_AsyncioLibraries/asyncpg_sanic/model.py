233757ac74cf22044ee49a8e42545543fddd2955
Jeremi Mucha <jam.sfinae@gmail.com>
2021-09-02 21:53:14 +0200

    Add sanic demo

--- AsyncioInPython/Ch4_AsyncioLibraries/asyncpg_sanic/model.py
+++ AsyncioInPython/Ch4_AsyncioLibraries/asyncpg_sanic/model.py
@@ -22,3 +22,46 @@ DELETE = 'DELETE FROM patron WHERE id=$1'
 EXISTS = "SELECT to_regclass('patron')"
 
 CACHE = LRU(max_size=65536)
+
+
+async def add_patron(conn, data: dict) -> int:
+    return await conn.fetchval(INSERT, data['name'], data['fav_dish'])
+
+
+async def update_patron(conn, id: int, data: dict) -> bool:
+    result = await conn.execute(UPDATE, data['name'], data['fav_dish'], id)
+    return result == 'UPDATE 1'
+
+
+async def delete_patron(conn, id: int) -> bool:
+    result = await conn.execute(DELETE, id)
+    return result == 'DELETE 1'
+
+
+# Note that this is the only operation that actually touches the cache directly.
+# This is because all the others - add/update/delete rely on the installed triggers.
+async def get_patron(conn, id: int) -> dict:
+    if id not in CACHE:
+        logger.info(f'id={id} Cache miss')
+        record = await conn.fetchrow(SELECT, id)
+        CACHE[id] = record and dict(record.items())
+    return CACHE[id]
+
+
+def db_event(conn, pid, channel, payload):
+    event = loads(payload)
+    logger.info('Got DB event:\n' + dumps(event, indent=4))
+    id = event['id']
+    if event['type'] == 'INSERT':
+        CACHE[id] = event['data']
+    elif event['type'] == 'UPDATE':
+        CACHE[id] = event['data']['new']
+    elif event['type'] == 'DELETE':
+        CACHE[id] = None
+
+
+async def create_table_if_missing(conn):
+    if not await conn.fetchval(EXISTS):
+        await conn.fetchval(CREATE_TABLE)
+        await create_notify_trigger(conn, channle='chan_patron')
+        await add_table_triggers(conn, table='patron')
--- AsyncioInPython/Ch4_AsyncioLibraries/asyncpg_sanic/sanic_demo.py
+++ AsyncioInPython/Ch4_AsyncioLibraries/asyncpg_sanic/sanic_demo.py
@@ -5,12 +5,52 @@ from sanic import Sanic
 from sanic.views import HTTPMethodView
 from sanic.response import json
 from util import Database
+from perf import aelapsed, aprofiler
+import model
 
 
 app = Sanic()
 
 
+@aelapsed
 async def new_patron(request):
     data = request.json
     id = await model.add_patron(app.pool, data)
     return json(dict(msg='ok', id=id))
+
+
+class PatronAPI(HTTPMethodView, metaclass=aprofiler):
+    async def get(self, request, id):
+        data = await model.get_patron(app.pool, id)
+        return json(data)
+
+    async def put(self, request, id):
+        data = request.json
+        ok = await model.update_patron(app.pool, id, data)
+        return json(dict(msg='ok' if ok else 'bad'))
+
+    async def delete(self, request, id):
+        ok = await model.delete_patron(app.pool, id)
+        return json(dict(msg='ok' if ok else 'bad'))
+
+    
+@app.listener('before_server_start')
+async def db_connect(app, loop):
+    app.db = Database('restaurant', owner=False)
+    app.pool = await app.db.connect()
+    await model.create_table_if_missing(app.pool)
+    await app.db.add_listener('chan_patron', model.db_event)
+
+
+@app.listener('after_server_stop')
+async def db_disconnect(app, loop):
+    await app.db.disconnect()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=8000)
+    args = parser.parse_args()
+    app.add_route(new_patron, '/patron', methods=['POST'])
+    app.add_route(PatronAPI.as_view(), '/patron/<id:int>')
+    app.run(host='0.0.0.0', port=args.port)
